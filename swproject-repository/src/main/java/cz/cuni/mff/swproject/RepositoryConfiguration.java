2d0757ceace7421923b9afd46fc4829ab6578084
Lukáš Kolek <luky.kolek@seznam.cz>
2019-03-10 00:34:23 +0100

    Revert "repository init"

--- swproject-repository/src/main/java/cz/cuni/mff/swproject/RepositoryConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cz.cuni.mff.swproject;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class RepositoryConfiguration {
-
-}
