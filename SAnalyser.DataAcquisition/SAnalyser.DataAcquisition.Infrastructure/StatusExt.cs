0a8623207f71badc9e7708bcc830fcc4d74e9575
Jaroslav Knotek <jaroslav.knotek@profinit.eu>
2019-03-24 21:26:15 +0100

    first draft of data acquisition module - console only

---
+++ SAnalyser.DataAcquisition/SAnalyser.DataAcquisition.Infrastructure/StatusExt.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using LinqToTwitter;
+
+namespace SAnalyser.DataAcquisition.Infrastructure
+{
+    public static class StatusExt
+    {
+        public static UniPost ToUnifiedPost(this Status status)
+        {
+            return UniPost.FromValues(status.Text);
+        }
+    }
+}
