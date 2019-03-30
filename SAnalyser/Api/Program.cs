79306022840ce26705b03857da3645f9265193c1
JaroslavKnotek <knotekjaroslav@email.cz>
2019-03-30 15:09:19 +0100

    slight changes

--- SAnalyser/Api/Program.cs
+++ SAnalyser/Api/Program.cs
@@ -12,6 +12,7 @@ namespace SAnalyser.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>();
     }
 }
