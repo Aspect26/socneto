2a056205258a753176b4504fec2d2609ab2a5daa
Julius Flimmel <flimmelj@gmail.com>
2019-04-24 11:20:02 +0200

    Added drawer content

--- frontend/lib/src/components/app_layout/app_layout_component.dart
+++ frontend/lib/src/components/app_layout/app_layout_component.dart
@@ -4,6 +4,7 @@ import 'package:angular_components/app_layout/material_temporary_drawer.dart';
 import 'package:angular_components/content/deferred_content.dart';
 import 'package:angular_components/material_button/material_button.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
+import 'package:angular_components/material_list/material_list_item.dart';
 import 'package:angular_components/material_toggle/material_toggle.dart';
 import 'package:sw_project/src/components/task_detail/task_detail_component.dart';
 import 'package:sw_project/src/components/tasks_list/tasks_list_component.dart';
@@ -28,6 +29,7 @@ import 'package:sw_project/src/models/Task.dart';
       MaterialPersistentDrawerDirective,
       MaterialTemporaryDrawerComponent,
       MaterialToggleComponent,
+      MaterialListItemComponent
     ],
     encapsulation: ViewEncapsulation.None
 )
