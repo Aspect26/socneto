244fe5a9a574c123cf370f1bed7147581fbb5274
Julius Flimmel <flimmelj@gmail.com>
2019-04-24 10:41:22 +0200

    Added material toolbar

--- frontend/lib/src/components/toolbar/toolbar_component.dart
+++ frontend/lib/src/components/toolbar/toolbar_component.dart
@@ -1,10 +1,16 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_icon/material_icon.dart';
 
 @Component(
     selector: 'toolbar',
     templateUrl: 'toolbar_component.html',
-    styleUrls: const ['toolbar_component.css'],
-    directives: []
+    styleUrls: const [
+      'toolbar_component.css',
+      'package:angular_components/app_layout/layout.scss.css'
+    ],
+    directives: [
+      MaterialIconComponent
+    ]
 )
 class ToolbarComponent {
   bool customWidth = false;
