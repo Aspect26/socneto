5151b0a8110e9df6df8ac8b5484a469c94c3d6c6
Julius Flimmel <flimmelj@gmail.com>
2019-04-24 10:54:16 +0200

    Renamed toolbar component to app layout component

--- frontend/lib/app_component.dart
+++ frontend/lib/app_component.dart
@@ -1,12 +1,12 @@
 import 'package:angular/angular.dart';
-import 'package:sw_project/src/components/toolbar/toolbar_component.dart';
+import 'package:sw_project/src/components/app_layout/app_layout_component.dart';
 
 
 @Component(
   selector: 'my-app',
   styleUrls: ['app_component.css'],
   templateUrl: 'app_component.html',
-  directives: [ToolbarComponent],
+  directives: [AppLayoutComponent],
 )
 class AppComponent {
 
