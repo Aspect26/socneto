dd14767ffc6910519267a964279d1a16c8d3b2c3
Julius Flimmel <flimmelj@gmail.com>
2019-04-24 10:51:42 +0200

    Added drawer

--- frontend/lib/src/components/toolbar/toolbar_component.dart
+++ frontend/lib/src/components/toolbar/toolbar_component.dart
@@ -1,5 +1,13 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/app_layout/material_persistent_drawer.dart';
+import 'package:angular_components/app_layout/material_temporary_drawer.dart';
+import 'package:angular_components/content/deferred_content.dart';
+import 'package:angular_components/material_button/material_button.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
+import 'package:angular_components/material_toggle/material_toggle.dart';
+import 'package:sw_project/src/components/task_detail/task_detail_component.dart';
+import 'package:sw_project/src/components/tasks_list/tasks_list_component.dart';
+import 'package:sw_project/src/models/Task.dart';
 
 @Component(
     selector: 'toolbar',
@@ -9,10 +17,27 @@ import 'package:angular_components/material_icon/material_icon.dart';
       'package:angular_components/app_layout/layout.scss.css'
     ],
     directives: [
-      MaterialIconComponent
-    ]
+      TasksListComponent,
+      TaskDetailComponent,
+
+      DeferredContentDirective,
+
+      MaterialIconComponent,
+      MaterialButtonComponent,
+      MaterialIconComponent,
+      MaterialPersistentDrawerDirective,
+      MaterialTemporaryDrawerComponent,
+      MaterialToggleComponent,
+    ],
+    encapsulation: ViewEncapsulation.None
 )
 class ToolbarComponent {
   bool customWidth = false;
   bool end = false;
+
+  @ViewChild(TaskDetailComponent) TaskDetailComponent taskDetailComponent;
+
+  taskSelected(Task task) {
+    taskDetailComponent.setTask(task);
+  }
 }
