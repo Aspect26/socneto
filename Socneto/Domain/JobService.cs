3f5d7557d2203462bab1d7257ebdb59efc8fd2e3
JaroslavKnotek <knotekjaroslav@email.cz>
2019-04-27 19:57:44 +0200

    data collector removed

--- Socneto/Domain/JobService.cs
+++ Socneto/Domain/JobService.cs
@@ -4,24 +4,21 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
-using Socneto.Coordinator.Domain.Interfaces;
 using Socneto.Coordinator.Domain.Models;
 
 namespace Socneto.Coordinator.Domain
 {
-    
+
     public class JobService : IJobService
     {
-        private readonly IDataCollector _dataCollector;
-        
+
         private readonly IResultProducer _producer;
         private readonly ILogger<JobService> _logger;
-        
-        
-        public JobService(IDataCollector dataCollector, IResultProducer producer, ILogger<JobService> logger)
+
+
+        public JobService(IResultProducer producer, ILogger<JobService> logger)
         {
-            _dataCollector = dataCollector;
-            
+
             _producer = producer;
             _logger = logger;
         }
@@ -29,31 +26,29 @@ namespace Socneto.Coordinator.Domain
 
         public async Task<JobSubmitResult> SubmitJob(JobSubmitInput jobInput)
         {
-            var page = await _dataCollector.CollectDataAsync(jobInput);
             var guid = Guid.NewGuid().ToString();
 
             _logger.LogInformation("Processing data");
 
-            foreach (var post in page.PostDataList)
+
+            var job = new JobDataRequest()
             {
-                var job = new JobResultWrapper()
+                JobId = guid,
+                Topic = jobInput.Topic
+            };
+
+            var json = JsonConvert.SerializeObject(job);
+
+
+            await _producer.ProduceAsync(
+                new Message()
                 {
-                    JobId = guid,
-                    Data = post,
-                    DataType = "post"
-                };
-                
-                var json = JsonConvert.SerializeObject(post);
+                    // this is not required
+                    Key = "Request",
+                    Value = json
+                }
+            );
 
-
-                await _producer.ProduceAsync(
-                    new Message()
-                    {
-                        Key = "Post",
-                        Value = json
-                    }
-                );
-            }
             return new JobSubmitResult() { JobId = guid };
         }
 
