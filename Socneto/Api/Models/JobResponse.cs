6b4413f766a40cfba50d2050114f1221e11bd30a
JaroslavKnotek <knotekjaroslav@email.cz>
2019-04-27 19:07:53 +0200

    Api changes

---
+++ Socneto/Api/Models/JobResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Socneto.Coordinator.Domain.Models;
+
+namespace Socneto.Coordinator.Api.Models
+{
+    public class JobResponse
+    {
+        public string JobId { get; set; }
+    }
+}
---
+++ Socneto/Domain/Models/PostData.cs
@@ -0,0 +1,7 @@
+namespace Socneto.Coordinator.Domain.Models
+{
+    public class PostData
+    {
+        public string Text { get; set; }
+    }
+}
