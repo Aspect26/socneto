9dc773b223539aaf71ea7245fb3b105c3281e9b3
JaroslavKnotek <knotekjaroslav@email.cz>
2019-05-02 20:27:58 +0200

    logging enabled

--- Coordinator/Socneto/Infrastructure/Kafka/KafkaProducer.cs
+++ Coordinator/Socneto/Infrastructure/Kafka/KafkaProducer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Socneto.Coordinator.Domain;
 using Socneto.Coordinator.Domain.Models;
@@ -9,15 +10,23 @@ namespace Socneto.Coordinator.Infrastructure.Kafka
 {
     public class KafkaProducer: IResultProducer
     {
+        private readonly ILogger<KafkaProducer> _logger;
         private readonly string _serverAddress;
         private readonly string _topicName;
 
-        public KafkaProducer(IOptions<KafkaOptions> kafkaOptionsObject)
+        public KafkaProducer(IOptions<KafkaOptions> kafkaOptionsObject, ILogger<KafkaProducer> logger)
         {
+            _logger = logger;
+            if(string.IsNullOrEmpty(kafkaOptionsObject.Value.ServerAddress))
+                throw new ArgumentNullException(nameof(kafkaOptionsObject.Value.ServerAddress));
             _serverAddress = kafkaOptionsObject.Value.ServerAddress;
+            
+            if (string.IsNullOrEmpty(kafkaOptionsObject.Value.ProduceDbStoreTopic))
+                throw new ArgumentNullException(nameof(kafkaOptionsObject.Value.ProduceDbStoreTopic));
             _topicName = kafkaOptionsObject.Value.ProduceDbStoreTopic;
 
         }
+
         public async Task ProduceAsync(Message message)
         {
             var config = new ProducerConfig { BootstrapServers = _serverAddress };
@@ -33,11 +42,11 @@ namespace Socneto.Coordinator.Infrastructure.Kafka
                         _topicName, KafkaMessage.ToKafka(message)
                         );
 
-                    Console.WriteLine($"delivered to: {deliveryReport.TopicPartitionOffset}");
+                    _logger.LogInformation($"delivered to: {deliveryReport.TopicPartitionOffset}");
                 }
                 catch (ProduceException<string, string> e)
                 {
-                    Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                    _logger.LogError($"failed to deliver message: {e.Message} [{e.Error.Code}]");
                 }
             }
         }
