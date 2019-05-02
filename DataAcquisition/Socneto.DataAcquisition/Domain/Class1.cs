5ac4cd08f065c69efe14bd44a113378ce9204a23
JaroslavKnotek <knotekjaroslav@email.cz>
2019-05-02 20:37:48 +0200

    consuming logging

--- DataAcquisition/Socneto.DataAcquisition/Domain/Class1.cs
+++ DataAcquisition/Socneto.DataAcquisition/Domain/Class1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.Design;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
@@ -13,19 +14,18 @@ namespace Socneto.DataAcquisition.Domain
     {
         private readonly IJobConsumer _consumer;
         private readonly IResultProducer _resultProducer;
-        private readonly string _consumeTopic;
-        private readonly string _produceTopic;
+        private readonly ILogger<StuffDoer> _logger;
 
-        public StuffDoer(IJobConsumer consumer, IResultProducer resultProducer )
+        public StuffDoer(IJobConsumer consumer, IResultProducer resultProducer, ILogger<StuffDoer> logger )
         {
             _consumer = consumer;
             _resultProducer = resultProducer;
-
-            
+            _logger = logger;
         }
 
         public async Task DoSomeRealWork()
         {
+            _logger.LogInformation("Consuming started");
             await _consumer.ConsumeAsync( ProduceRecords, CancellationToken.None );
         }
 
@@ -35,16 +35,16 @@ namespace Socneto.DataAcquisition.Domain
             // TODO i should deeserialize right at the consumer or register deserializer
             var taskInput = JsonConvert.DeserializeObject<TaskInput>(input);
 
+            _logger.LogInformation($"Generating 10 mock records for: '{taskInput.Topic}'");
             //... todo
 
             for (int i = 0; i < 10; i++)
             {
-
-                var text = RandomString(random.Next(50, 100));
+                var text =taskInput.Topic +"-"+ RandomString(random.Next(50, 100));
                 var user = RandomString(random.Next(5, 15));
                 var source = "twitter";
 
-                var post = UniPost.FromValues(text, source, user);
+                var post =  UniPost.FromValues(text, source, user);
                 var serialized = JsonConvert.SerializeObject(post);
                 var message = new Message()
                 {
