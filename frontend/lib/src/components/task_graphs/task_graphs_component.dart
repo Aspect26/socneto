eae381a93a08a894ac122b99b3bbe2f6725ac8a4
Julius Flimmel <flimmelj@gmail.com>
2019-05-07 16:22:44 +0200

    Line chart mock data update

--- frontend/lib/src/components/task_graphs/task_graphs_component.dart
+++ frontend/lib/src/components/task_graphs/task_graphs_component.dart
@@ -19,20 +19,30 @@ import 'dart:js';
 )
 class TaskGraphsComponent extends AfterViewInit {
 
+  var data = [
+    {
+      'keyword': 'Game of Thrones',
+      'data': [{'y': 0.3, 'date': "2019-01-04"}, {'y': 0.8, 'date': "2019-02-04"}, {'y': 0.5, 'date': "2019-05-04"}],
+    },
+    {
+      'keyword': 'Star Wars',
+      'data': [{'y': 0.5, 'date': "2019-01-04"}, {'y': 0.2, 'date': "2019-04-04"}, {'y': 0.7, 'date': "2019-05-04"}],
+    },
+    {
+      'keyword': 'Internet',
+      'data': [{'y': 0.5, 'date': "2019-01-01"}, {'y': 0.2, 'date': "2019-02-04"}, {'y': 0.7, 'date': "2019-03-04"}, {'y': 0.1, 'date': "2019-04-04"}, {'y': 0.42, 'date': "2019-05-04"}, {'y': 0.3, 'date': "2019-06-04"}, {'y': 0.1, 'date': "2019-07-04"}]
+    }
+  ];
+
   @override
   Future<Null> ngAfterViewInit() async {
-
 
     var width = 800;
     var height = 200;
 
-    var dataset = [{'y': 0.3, 'date': "2019-01-04"}, {'y': 0.8, 'date': "2019-02-04"}, {'y': 0.5, 'date': "2019-05-04"}];
-    var dataset2 = [{'y': 0.5, 'date': "2019-01-04"}, {'y': 0.2, 'date': "2019-04-04"}, {'y': 0.7, 'date': "2019-05-04"}];
-    var dataset3 = [{'y': 0.5, 'date': "2019-01-01"}, {'y': 0.2, 'date': "2019-02-04"}, {'y': 0.7, 'date': "2019-03-04"},
-      {'y': 0.1, 'date': "2019-04-04"}, {'y': 0.42, 'date': "2019-05-04"}, {'y': 0.3, 'date': "2019-06-04"}, {'y': 0.1, 'date': "2019-07-04"}, ];
+    var datasets = data.map( (element) => element['data']).toList();
+    var dataLabels = data.map( (element) => element['keyword']).toList();
 
-    var datasets = [dataset, dataset2, dataset3];
-    var dataLabels = ["Game of Thrones", "Star Wars", "Internet"];
 
     context.callMethod('createLineChart', [".graph-line-chart", new JsObject.jsify(datasets), new JsObject.jsify(dataLabels), width, height]);
 
