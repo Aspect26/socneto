4c6373f812f4385527a47bf04c08c46a4214ccac
JaroslavKnotek <knotekjaroslav@email.cz>
2019-05-14 18:59:49 +0200

    api extended, route changed

--- Coordinator/Socneto/Api/Models/JobResultResponse.cs
+++ Coordinator/Socneto/Api/Models/JobResultResponse.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Socneto.Coordinator.Api.Models
 {
     public class JobResultResponse
     {
         public string InputQuery { get; set; }
+
+        public Guid JobId { get; set; }
+
+
 
         public List<PostDto> Posts { get; set; }
 
--- Coordinator/Socneto/Api/Models/JobStatusResponse.cs
+++ Coordinator/Socneto/Api/Models/JobStatusResponse.cs
@@ -6,6 +6,7 @@ namespace Socneto.Coordinator.Api.Models
     {
         public Guid JobId { get; set; }
         public int UserId { get; set; }
+        public string JobName { get; set; }
         public bool HasFinished { get; set; }
         public DateTime StartedAt { get; set; }
         public DateTime? FinishedAt { get; set; }
