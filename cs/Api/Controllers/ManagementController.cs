cd5af7a4bcb0f160be8e22970d753265e729abfd
JaroslavKnotek <knotekjaroslav@email.cz>
2019-06-04 22:20:47 +0200

    logger added

--- cs/Api/Controllers/ManagementController.cs
+++ cs/Api/Controllers/ManagementController.cs
@@ -14,11 +14,13 @@ namespace Socneto.Api.Controllers
     public class ManagementController : ControllerBase
     {
         private readonly ILogger<KafkaProducer> _kafkaLogger;
+        private readonly ILogger<ManagementController> _logger;
 
 
-        public ManagementController(ILogger<KafkaProducer> kafkaLogger)
+        public ManagementController(ILogger<KafkaProducer> kafkaLogger, ILogger<ManagementController> logger)
         {
             this._kafkaLogger = kafkaLogger;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -36,19 +38,26 @@ namespace Socneto.Api.Controllers
 
         [HttpPost]
         [Route("api/produce")]
-        public async Task< ActionResult> Produce([FromBody]ProduceRequest request)
+        public async Task<ActionResult> Produce([FromBody] ProduceRequest request)
         {
-            var kafkaOptions = Options.Create(new KafkaOptions { ServerAddress = request.ServerAddress });
-            var taskOptions =
-                Options.Create(new TaskOptions {ConsumeTaskTopic = "n/a", ProduceTopic = request.KafkaTopic});
-            var kafka = new KafkaProducer(kafkaOptions, taskOptions, _kafkaLogger);
-
-            var message = new Message()
+            try
             {
-                Key = "key",
-                Value = request.Message
-            };
-            await kafka.ProduceAsync(message);
+                var kafkaOptions = Options.Create(new KafkaOptions {ServerAddress = request.ServerAddress});
+                var taskOptions =
+                    Options.Create(new TaskOptions {ConsumeTaskTopic = "n/a", ProduceTopic = request.KafkaTopic});
+                var kafka = new KafkaProducer(kafkaOptions, taskOptions, _kafkaLogger);
+
+                var message = new Message()
+                {
+                    Key = "key",
+                    Value = request.Message
+                };
+                await kafka.ProduceAsync(message);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("error :{message}",e.Message);
+            }
 
             return Ok();
         }
