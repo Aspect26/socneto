49ff91194c673aae1f214180f26adacb423094fb
Julius Flimmel <flimmelj@gmail.com>
2019-06-09 11:43:08 +0200

    Frontend - refactor - changed components directory hierarchy to match their hierarchy in the html

--- dart/frontend/web/main.dart
+++ dart/frontend/web/main.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
-import 'package:sw_project/app_component.template.dart' as ng;
+import 'package:sw_project/src/components/app_component/app_component.template.dart' as ng;
 
 import 'main.template.dart' as self;
 
