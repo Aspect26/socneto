bc896032cf3de68b485adde9cff0cf28a922339b
JaroslavKnotek <knotekjaroslav@email.cz>
2019-06-29 10:08:16 +0200

    initial

---
+++ cs/JobManagementService/Domain/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace Domain
+{
+    public class RequestListenerHostedService:IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RequestListener: IRequestListener
+    {
+
+    }
+
+    public interface IRequestListener
+    {
+
+    }
+
+    public class RegistrationRequestMessage
+    {
+
+    }
+}
