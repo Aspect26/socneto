95ad01082873fd799840a14a6f4f1c61bad2eb1b
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2019-06-29 12:21:48 +0200

    registration covered

--- cs/JobManagementService/Domain/Class1.cs
+++ cs/JobManagementService/Domain/Class1.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain.Models;
 using Microsoft.Extensions.Hosting;
+// ReSharper disable All
 
 namespace Domain
 {
@@ -41,23 +45,84 @@ namespace Domain
         CreateChannelResult CreateChannel(MessageBrokerChannelModel channelModel);
 
     }
-
-    public class CreateChannelResult
+    
+    public interface ISubscribedComponentManager
     {
-        public CreateChannelResult(string channelName)
+        void SubscribeComponent(ComponentRegistrationModel componentRegistrationModel);
+
+        // todo
+        // Task PushJobConfiguration();
+    }
+    
+
+    public class SubscribedComponentManager : ISubscribedComponentManager
+    {
+        private readonly IComponentRegistry _componentRegistry;
+
+        public SubscribedComponentManager(IComponentRegistry componentRegistry)
         {
-            ChannelName = channelName;
+            _componentRegistry = componentRegistry;
+        }
+        public void SubscribeComponent(ComponentRegistrationModel componentRegistrationModel)
+        {
+           var registered= _componentRegistry.AddOrUpdate(componentRegistrationModel);
+            if (!registered)
+            {
+                throw new InvalidOperationException(
+                    $"Device {componentRegistrationModel.ComponentId} already exists");
+            }
+        }
+    }
+
+    public interface IComponentRegistry
+    {
+        bool AddOrUpdate(ComponentRegistrationModel componentRegistrationModel);
+    }
+
+    public class ComponentRegistry :IComponentRegistry
+    {
+        private readonly ConcurrentDictionary<string, List<SubscribedComponent>> _registeredComponentsTypes 
+            = new ConcurrentDictionary<string, List<SubscribedComponent>>();
+
+        private readonly ConcurrentDictionary<string, SubscribedComponent> _registeredComponents
+            =new ConcurrentDictionary<string, SubscribedComponent>();
+
+        public bool AddOrUpdate(ComponentRegistrationModel componentRegistrationModel)
+        {
+            var subscribedComponent = new SubscribedComponent(componentRegistrationModel.ComponentType,
+                componentRegistrationModel.ChannelId);
+
+            if (!_registeredComponents.TryAdd(componentRegistrationModel.ComponentId, subscribedComponent))
+            {
+                // Already exists
+                return false;
+            }
+            
+            var key = componentRegistrationModel.ComponentType;
+            _registeredComponentsTypes.AddOrUpdate(key,
+                k => new List<SubscribedComponent>() { subscribedComponent},
+                (k, existingList) =>
+                {
+                    existingList.Add(subscribedComponent);
+                    return existingList;
+                });
+            return true;
         }
 
-        public string ChannelName { get;  }
+        
+
+        public IList<SubscribedComponent> GetAllByType(string componentType)
+        {
+            if (_registeredComponentsTypes.TryGetValue(componentType, out var list))
+            {
+                return list;
+            }
+            else
+            {
+                return new List<SubscribedComponent>();
+            }
+        }
     }
 
     
-    public interface IComponentConfigSubscriber
-    {
-        void SubscribeComponent(ComponentRegisterModel componentRegisterModel);
-    }
-
-
-
 }
---
+++ cs/JobManagementService/Domain/Models/ComponentRegistrationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Models
+{
+    public class ComponentRegistrationModel
+    {
+        public string ComponentId { get; }
+        public string ChannelId { get; }
+        public string ComponentType { get; }
+        
+        public ComponentRegistrationModel(string componentId, string channelId, string requestComponentType)
+        {
+            ComponentId = componentId;
+            ChannelId = channelId;
+            ComponentType = requestComponentType;
+        }
+    }
+}
