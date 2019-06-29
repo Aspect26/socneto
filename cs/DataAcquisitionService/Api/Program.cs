63b34f80487f68f3e7b218e951b0fba593dbe095
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2019-06-29 22:18:35 +0200

    project structrue introduced

---
+++ cs/DataAcquisitionService/Api/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Domain.Registration;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Api
+{
+    public class Program
+    {
+        public static async Task MainAsync(string[] args)
+        {
+            var app = CreateWebHostBuilder(args)
+                .Build();
+
+            await InitializeApplication(app);
+
+            app.Run();
+        }
+
+        private static async Task InitializeApplication(IWebHost app)
+        {
+            var registration = app.Services.GetRequiredService<IRegistrationService>();
+
+            var componentOptions = app.Services.GetRequiredService<IOptions<ComponentOptions>>();
+            
+            var registrationRequest = new RegistrationRequest()
+            {
+                ComponentId = componentOptions.Value.ComponentId,
+                ComponentType = componentOptions.Value.ComponentType
+            };
+
+            await registration.Register(registrationRequest);
+        }
+
+        public static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();    
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ cs/DataAcquisitionService/Infrastructure/Kafka/KafkaProducer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Domain;
+using Domain.Abstract;
+
+namespace Infrastructure.Kafka
+{
+    public class KafkaProducer : IMessageBrokerProducer
+    {
+        private readonly ILogger<KafkaProducer> _logger;
+        private readonly string _serverAddress;
+        
+        public KafkaProducer(IOptions<KafkaOptions> kafkaOptionsObject, ILogger<KafkaProducer> logger)
+        {
+
+            if (string.IsNullOrEmpty(kafkaOptionsObject.Value.ServerAddress))
+                throw new ArgumentNullException(nameof(kafkaOptionsObject.Value.ServerAddress));
+            _logger = logger;
+            _serverAddress = kafkaOptionsObject.Value.ServerAddress;
+            
+        }
+        public async Task ProduceAsync(string topic, MessageBrokerMessage message)
+        {
+            var config = new ProducerConfig { BootstrapServers = _serverAddress };
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                try
+                {
+                    // Note: Awaiting the asynchronous produce request below prevents flow of execution
+                    // from proceeding until the acknowledgement from the broker is received (at the 
+                    // expense of low throughput).
+                    var deliveryReport = await producer.ProduceAsync(
+                        topic, KafkaMessage.FromMessageBrokerMessage(message)
+                        );
+
+                    _logger.LogInformation($"delivered to: {deliveryReport.TopicPartitionOffset}");
+                }
+                catch (ProduceException<string, string> e)
+                {
+                    _logger.LogError($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                }
+            }
+        }
+    }
+
+}
