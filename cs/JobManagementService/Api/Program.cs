ef5f27078e91236c6dc04628b0878de7dfe7335e
JaroslavKnotek <knotekjaroslav@email.cz>
2019-06-30 14:13:29 +0200

    data acq service added and integrated with kafka

--- cs/JobManagementService/Api/Program.cs
+++ cs/JobManagementService/Api/Program.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Api
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        public static async Task MainAsync(string[] args)
+        {
+
+            await Task.Delay(TimeSpan.FromMinutes(1));
+            var app = CreateWebHostBuilder(args).Build();
+            app.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
