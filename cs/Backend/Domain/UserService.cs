1ebe879561aa66232e3331976745a526cd08afb7
Julius Flimmel <flimmelj@gmail.com>
2019-07-16 13:30:18 +0200

    Added authorization to the backend

---
+++ cs/Backend/Domain/UserService.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Socneto.Domain.Models;
+
+namespace Socneto.Domain
+{
+    public class UserService : IUserService
+    {
+        // TODO: read this from DB
+        private readonly List<User> _users = new List<User>
+        { 
+            new User { Id = 1, Username = "admin", Password = "admin" } 
+        };
+        
+        public async Task<User> Authenticate(string username, string password)
+        {
+            var user = await Task.Run(() => _users.SingleOrDefault(x => x.Username == username && x.Password == password));
+
+            if (user == null)
+                return null;
+
+            // authentication successful so return user details without password
+            user.Password = null;
+            
+            return user;
+        }
+    }
+}
