d70a74324fe954e130471cb5497fa0a9284a4caf
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2019-07-18 20:04:38 +0200

    python related changes

--- Analyser/main.py
+++ Analyser/main.py
@@ -1,25 +1,30 @@
 from time import sleep
+import json
+import threading
 import consumer
 import producer
-import argparse
 import Analyser.language_recognition.language_recognizer as lr
 import Analyser.sentiment_analysis.sentiment_analyser as sa
 
 if __name__ != "__main__":
     exit()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--server", type=str, help="kafka server")
-parser.add_argument("--consume_topic", type=str, help="consumer topic name")
-parser.add_argument("--produce_topic", type=str, help="producer topic name")
+with open('config.json', 'r') as file:
+    config = json.load(file)
+serverAddress = config['kafkaOptions']['serverAddress']
+registrationTopicName= config['registrationRequestOptions']['registrationChannelName']
 
-args = parser.parse_args()
+kafka_producer = producer.Producer(serverAddress)
+update_channel_name = config["componentOptions"]["updateChannelName"]
 
-kafka_producer = producer.Producer(args.server, args.produce_topic)
 kafka_producer.subscribe_logging(lambda x: print(x))
 
+# the similarity is not intentional
+registrationRequest = config['componentOptions']
 
-def produce_sentiment(data):
+kafka_producer.produce(registrationTopicName, registrationRequest)
+
+def analyse_post(data):
     text = data['text']
     sentiment_dic = {
         0: "negative",
@@ -30,23 +35,32 @@ def produce_sentiment(data):
 
     language = lr.detect_language(text)
 
-    analysed_data = {
+    return {
         "originalData": data,
         "analysis": {
             "sentimentText": sentiment_dic[sentiment_value],
             "sentimentVal": sentiment_value,
-            "keywords":["kw1","kw2"]
+            "keywords":["kw1","kw2"],
             "language": language
         }
     }
 
-    kafka_producer.produce(analysed_data)
+def produce_analysis(post):
+    analysed = analyse_post(post)
+    producer.produce(update_channel_name, analysed)
 
+def process_submitted_job(job_config):
+    def start_job():
+        input_channel = job_config['inputMessageBrokerChannel']
+        job_consumer = consumer.Consumer(serverAddress, input_channel)
+        while True:
+            job_consumer.subscribe_logging(lambda x: print(x))
+            job_consumer.consume(produce_analysis)
+    thread1 = threading.Thread(target = start_job)
+    thread1.start()
 
-kafka_producer.produce({"info": "test produce"})
-
-kafka_consumer = consumer.Consumer(args.server, args.consume_topic)
-kafka_consumer.subscribe_logging(lambda x: print(x))
+config_consumer = consumer.Consumer(serverAddress, update_channel_name)
 while True:
-    kafka_consumer.consume(produce_sentiment)
-    sleep(1)
+    config_consumer.subscribe_logging(lambda x: print(x))
+    config_consumer.consume(process_submitted_job)
+
