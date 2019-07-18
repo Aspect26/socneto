8f91570b99ec68f5cb808e2f328b1cd71f492e37
JaroslavKnotek <knotekjaroslav@email.cz>
2019-07-18 20:04:38 +0200

    python related changes

--- Analyser/Infrastructure/producer.py
+++ Analyser/Infrastructure/producer.py
@@ -3,16 +3,15 @@ from kafka import KafkaProducer
 
 
 class Producer:
-    def __init__(self, server, topic):
+    def __init__(self, server):
         self._server = server
-        self._topic = topic
         self._logger = lambda x: x
         self.producer = KafkaProducer(bootstrap_servers=[server],
                                       value_serializer=lambda x: dumps(x).encode('utf-8'))
 
-    def produce(self, data_dic):
+    def produce(self, topic, data_dic):
         self._logger("producing to {}".format(self._topic))
-        self.producer.send(self._topic, value=data_dic)
+        self.producer.send(topic, value=data_dic)
         self._logger("produced {}".format(data_dic))
 
     def subscribe_logging(self, logger):
