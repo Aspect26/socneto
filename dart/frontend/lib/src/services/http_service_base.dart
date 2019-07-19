6e26ff80d79c702ae7c04e6e5cfe86d50ad25d9a
Julius Flimmel <flimmelj@gmail.com>
2019-07-19 13:14:52 +0200

    Frontend - removed print

--- dart/frontend/lib/src/services/http_service_base.dart
+++ dart/frontend/lib/src/services/http_service_base.dart
@@ -18,7 +18,6 @@ class HttpServiceBase {
   }
 
   Future<T> post<T>(String path, Map data, Function(Map) mapJson) async {
-    print("POST");
     var response = await this.httpPost(path, data);
     return this._extractJson<T>(response, mapJson);
   }
