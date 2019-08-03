7f39646cab452b2a851fa8e2f4290cf11ed8c2fe
JaroslavKnotek <knotekjaroslav@email.cz>
2019-08-03 20:48:30 +0200

    static data generator implemented

---
+++ cs/DataAcquisitionService/Infrastructure/DataGenerator/RandomGeneratorOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Infrastructure.DataGenerator
+{
+    public class RandomGeneratorOptions
+    {
+        public TimeSpan DownloadDelay { get; set; }
+        public int Seed { get; set; }
+    }
+
+    public class StaticGeneratorOptions
+    {
+        public TimeSpan DownloadDelay { get; set; }
+        public int Seed { get; set; }
+
+        public string StaticDataPath { get; set; }
+    }
+}
