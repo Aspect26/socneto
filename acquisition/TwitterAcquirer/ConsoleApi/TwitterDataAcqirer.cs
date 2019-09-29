7ef18733b5059734913e63f31d8f8498b26b0644
JaroslavKnotek <knotekjaroslav@email.cz>
2019-09-29 13:39:19 +0200

    restructuralization of acquirers

---
+++ acquisition/TwitterAcquirer/ConsoleApi/TwitterDataAcqirer.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Acquisition;
+using Domain.Model;
+using LinqToTwitter;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApi
+{
+    public class TwitterDataAcqirer //: IDataAcquirer
+    {
+        //private readonly TwitterContext _twitterContext;
+
+        //public TwitterDataAcqirer(TwitterContext twitterContext)
+        //{
+        //    _twitterContext = twitterContext;
+        //}
+
+        //public async Task<DataAcquirerOutputModel> AcquireBatchAsync(DataAcquirerInputModel acquirerInputModel, CancellationToken cancellationToken)
+        //{
+        //    var searchResponse =
+        //        await
+        //        (from search in _twitterContext.Search
+        //         where search.Type == SearchType.Search &&
+        //               search.Query == "\"LINQ to Twitter\""
+        //         select search)
+        //        .SingleOrDefaultAsync();
+
+        //    if (searchResponse != null && searchResponse.Statuses != null)
+        //        searchResponse.Statuses.ForEach(tweet =>
+        //            Console.WriteLine(
+        //                "User: {0}, Tweet: {1}",
+        //                tweet.User.ScreenNameResponse,
+        //                tweet.Text));
+        //}
+    }
+}
