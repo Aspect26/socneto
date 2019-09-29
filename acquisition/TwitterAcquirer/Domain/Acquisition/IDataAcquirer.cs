35b96d971dcd4a265c358b3548faec1c060cf36a
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2019-09-29 13:39:19 +0200

    restructuralization of acquirers

---
+++ acquisition/TwitterAcquirer/Domain/Acquisition/IDataAcquirer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Model;
+
+namespace Domain.Acquisition
+{
+    
+
+    public interface IDataAcquirer
+    {
+        Task<DataAcquirerOutputModel> AcquireBatchAsync(DataAcquirerInputModel acquirerInputModel, 
+            CancellationToken cancellationToken);
+        
+    }
+
+    
+
+
+}
