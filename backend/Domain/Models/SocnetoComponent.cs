4a6ef98bad9b3f88ad573147b522c15da30e413e
Julius Flimmel <flimmelj@gmail.com>
2019-10-22 19:02:32 +0200

    BE - analyse component dtos now send also analysis format

--- backend/Domain/Models/SocnetoComponent.cs
+++ backend/Domain/Models/SocnetoComponent.cs
@@ -1,17 +1,25 @@
+using Newtonsoft.Json.Linq;
+
 namespace Socneto.Domain.Models
 {
+    public enum SocnetoComponentType
+    {
+        DATA_ANALYSER,
+        DATA_ACQUIRER
+    }
+    
     public class SocnetoComponent
     {
         
         public string Id { get; set; }
         
-        public string Type { get; set; }
+        public string SocnetoComponentType { get; set; }
         
         public string InputChannelName { get; set; }
         
         public string UpdateChannelName { get; set; }
 
-        // public string Attributes { get; set; }
+        public JObject Attributes { get; set; }
         
     }
 }
