28d0e8533d02c90f376d99b2792e7726a46a7d05
Julius Flimmel <flimmelj@gmail.com>
2019-10-24 11:39:45 +0200

    FE+BE - removed unnecesary logs

--- backend/Api/Controllers/JobController.cs
+++ backend/Api/Controllers/JobController.cs
@@ -33,7 +33,6 @@ namespace Socneto.Api.Controllers
         [Route("api/job/{jobId:guid}/status")]
         public async Task<ActionResult<JobDto>> GetJobDetail([FromRoute]Guid jobId)
         {
-            _logger.LogInformation("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
             if (! await IsAuthorizedToSeeJob(jobId))
                 return Unauthorized();
             
@@ -60,38 +59,10 @@ namespace Socneto.Api.Controllers
         [Route("api/job/{jobId:guid}/analysis")]
         public async Task<ActionResult<Task<IList<IList<DataPoint>>>>> GetJobAnalysis([FromRoute]Guid jobId, [FromBody] CreateChartDefinitionRequest chartDefinition)
         {
-            _logger.LogInformation("BBBBB");
-            /*
             if (! await  IsAuthorizedToSeeJob(jobId))
                 return Unauthorized();
 
             var data = await _storageService.GetAnalyses();
-            */
-
-            var now = DateTime.Now;
-            var data = new List<List<DataPoint>>()
-            {
-                new List<DataPoint>()
-                {
-                    new List<dynamic>() { now.AddMinutes(-1), 1 },
-                    new List<dynamic>() { now.AddMinutes(-2), 0 },
-                    new List<dynamic>() { now.AddMinutes(-3), 0 },
-                    new List<dynamic>() { now.AddMinutes(-4), 1 },
-                    new List<dynamic>() { now.AddMinutes(-5), 0 }
-                },
-                
-                new List<DataPoint>()
-                {
-                    new List<dynamic>() { now.AddMinutes(-1), 0.98 },
-                    new List<dynamic>() { now.AddMinutes(-2), 0.95 },
-                    new List<dynamic>() { now.AddMinutes(-3), 0.99 },
-                    new List<dynamic>() { now.AddMinutes(-4), 0.93 },
-                    new List<dynamic>() { now.AddMinutes(-5), 0.90 }
-                },
-            };
-            
-            _logger.LogInformation("AAAAAAAAAAAAAAA");
-            
             return Ok(data);
         }
 
