9fafcbbb2e92baedd84101166e47a0c85e77e79b
Julius Flimmel <flimmelj@gmail.com>
2019-10-30 09:53:51 +0100

    DA - mock analyser now analyses emoticons

--- analysers/MockAnalyserService/Domain/Analyser/MockAnalyser.cs
+++ analysers/MockAnalyserService/Domain/Analyser/MockAnalyser.cs
@@ -13,9 +13,41 @@ namespace Domain.Analyser
     
     public class MockAnalyser : IAnalyser
     {
+        
+        private Dictionary<string, double> _emoticonsPolarity = new Dictionary<string, double>
+        {
+            { ":D", 1.0f },
+            { ":)", 1.0f },
+            { ":|", 0.0f },
+            { ":(", -1.0f },
+            { ":'", -1.0f },
+        };
+        
+        private Dictionary<string, double> _emoticonsAccuracy = new Dictionary<string, double>
+        {
+            { ":D", 1.0f },
+            { ":)", 0.8f },
+            { ":|", 1.0f },
+            { ":(", 0.8f },
+            { ":'", 1.0f },
+        };
+        
         public Task<Analysis> AnalyzePost(UniPost post)
         {
             var random = new Random(post.Text.GetHashCode());
+            double polarity;
+            double accuracy;
+
+            var knownEmoticonInText = GetKnownEmoticon(post.Text);
+            
+            if (knownEmoticonInText != null)
+            {
+                (polarity, accuracy) = AnalyzeEmoticon(knownEmoticonInText);
+            }
+            else
+            {
+                (polarity, accuracy) = AnalyzeRandom(random);
+            }
 
             var analysis = new Analysis
             {
@@ -23,16 +55,43 @@ namespace Domain.Analyser
                 {
                     ["polarity"] = new AnalysisValue
                     {
-                        Value = (random.NextDouble() - 0.5) * 2, ValueType = AnalysisValueType.Number
+                        Value = polarity, ValueType = AnalysisValueType.Number
                     },
                     ["accuracy"] = new AnalysisValue
                     {
-                        Value = random.NextDouble()
+                        Value = accuracy
                     }
                 }
             };
 
             return Task.FromResult(analysis);
+        }
+
+        private string GetKnownEmoticon(string text)
+        {
+            var knownEmoticons = _emoticonsPolarity.Keys;
+            foreach (var knownEmoticon in knownEmoticons)
+            {
+                if (text.Contains(knownEmoticon))
+                {
+                    return knownEmoticon;
+                }
+            }
+
+            return null;
+        }
+
+        private (double polarity, double accuracy) AnalyzeEmoticon(string emoticon)
+        {
+            _emoticonsPolarity.TryGetValue(emoticon, out var polarity);
+            _emoticonsAccuracy.TryGetValue(emoticon, out var accuracy);
+            
+            return (polarity, accuracy);
+        }
+        
+        private (double polarity, double accuracy) AnalyzeRandom(Random random)
+        {
+            return ((random.NextDouble() - 0.5) * 2, random.NextDouble());
         }
     }
 }
