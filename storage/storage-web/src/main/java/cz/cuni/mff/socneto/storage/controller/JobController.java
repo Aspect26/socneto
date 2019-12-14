a774bdf3e441d140ed6969bfbf1dddfed2b4509a
Julius Flimmel <flimmelj@gmail.com>
2019-12-14 17:29:10 +0100

    Fixed repository call + auto creating job view when creating job

--- storage/storage-web/src/main/java/cz/cuni/mff/socneto/storage/controller/JobController.java
+++ storage/storage-web/src/main/java/cz/cuni/mff/socneto/storage/controller/JobController.java
@@ -1,11 +1,17 @@
 package cz.cuni.mff.socneto.storage.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import cz.cuni.mff.socneto.storage.internal.api.dto.JobDto;
+import cz.cuni.mff.socneto.storage.internal.api.dto.JobViewDto;
 import cz.cuni.mff.socneto.storage.internal.api.service.JobDtoService;
+import cz.cuni.mff.socneto.storage.internal.api.service.JobViewDtoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +20,7 @@ import java.util.UUID;
 public class JobController {
 
     private final JobDtoService jobDtoService;
+    private final JobViewDtoService jobViewDtoService;
 
     @GetMapping("/jobs")
     public List<JobDto> getJobsByUsername(@RequestParam(value = "username", required = false) String username) {
@@ -31,6 +38,15 @@ public class JobController {
 
     @PostMapping("/jobs")
     public JobDto saveJob(@Valid @RequestBody JobDto job) {
+        JobViewDto jobViewDto = new JobViewDto();
+        jobViewDto.setJobId(job.getJobId());
+        try {
+            jobViewDto.setViewConfiguration((ObjectNode) new ObjectMapper().readTree("{ \"chartDefinitions\": []}"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jobViewDtoService.save(jobViewDto);
+
         return jobDtoService.save(job);
     }
 
