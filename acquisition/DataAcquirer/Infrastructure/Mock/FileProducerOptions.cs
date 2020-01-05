84a4a1965d8b6331e857d07716e4e69ae3488c16
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2020-01-05 23:24:21 +0100

    fix config

---
+++ acquisition/DataAcquirer/Infrastructure/Mock/FileProducerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Infrastructure.DataGenerator
+{
+    public class FileProducerOptions
+    {
+        [Required]
+        public string DestinationFilePath { get; set; }
+    }
+}
--- acquisition/DataAcquirer/Infrastructure/Twitter/TwitterContextWrapper.cs
+++ acquisition/DataAcquirer/Infrastructure/Twitter/TwitterContextWrapper.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Infrastructure.Twitter.Abstract;
@@ -22,6 +23,23 @@ namespace Infrastructure.Twitter
             ulong maxId = ulong.MaxValue,
             ulong sinceId = 1)
         {
+#warning hardcoded
+            return new Search()
+            {
+                Statuses = new List<Status>
+                {
+                    new Status
+            {
+                StatusID = 1,
+                FullText = $"test text {1}",
+                Lang = "en",
+                UserID = 0,
+                CreatedAt = DateTime.Now
+            }
+        }
+            };
+
+
             var combinedSearchResults = new List<Status>();
 
             // HOTFIX Null language results in exception saying that 
