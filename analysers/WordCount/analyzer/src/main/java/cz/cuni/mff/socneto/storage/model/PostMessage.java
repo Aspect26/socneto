b0c82cf2d0182cb8b7c5ff7c495e03ad79bec824
Lukas Kolek <luky.kolek@seznam.cz>
2020-01-12 10:52:05 +0100

    Storage and Analyzers: logging and hashtag

--- analysers/WordCount/analyzer/src/main/java/cz/cuni/mff/socneto/storage/model/PostMessage.java
+++ analysers/WordCount/analyzer/src/main/java/cz/cuni/mff/socneto/storage/model/PostMessage.java
@@ -1,11 +1,13 @@
 package cz.cuni.mff.socneto.storage.model;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Data
+@Builder
 public class PostMessage {
     private UUID postId;
     private String originalPostId;
--- storage/storage-web/src/main/java/cz/cuni/mff/socneto/storage/controller/UserController.java
+++ storage/storage-web/src/main/java/cz/cuni/mff/socneto/storage/controller/UserController.java
@@ -3,8 +3,11 @@ package cz.cuni.mff.socneto.storage.controller;
 import cz.cuni.mff.socneto.storage.internal.api.dto.UserDto;
 import cz.cuni.mff.socneto.storage.internal.api.service.UserDtoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserController {
@@ -13,6 +16,7 @@ public class UserController {
 
     @GetMapping("/users")
     public UserDto getUserByUsername(@RequestParam("username") String username) {
+        log.error("Errror: " + username);
         return userDtoService.find(username);
     }
 
