ed8f416dc3ecfcdb8347c5786d1df892443d7ae3
Lukas Kolek <luky.kolek@seznam.cz>
2020-01-12 10:52:05 +0100

    Storage and Analyzers: logging and hashtag

--- analysers/WordCount/analyzer/src/main/java/cz/cuni/mff/socneto/storage/model/AnalysisMessage.java
+++ analysers/WordCount/analyzer/src/main/java/cz/cuni/mff/socneto/storage/model/AnalysisMessage.java
@@ -1,5 +1,7 @@
 package cz.cuni.mff.socneto.storage.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
@@ -7,6 +9,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @Data
+@Builder
 public class AnalysisMessage {
     private UUID postId;
     private UUID jobId;
@@ -14,6 +17,7 @@ public class AnalysisMessage {
     private Map<String, AnalysisResult> results;
 
     @Data
+    @Builder
     public static class AnalysisResult {
         private Double numberValue;
         private String textValue;
