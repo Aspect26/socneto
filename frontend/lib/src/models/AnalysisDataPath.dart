a6aa179a55d81779176f63e776f3bc1615c01f07
Julius Flimmel <flimmelj@gmail.com>
2020-01-27 12:22:01 +0100

    FE - updated analysis requests

--- frontend/lib/src/models/AnalysisDataPath.dart
+++ frontend/lib/src/models/AnalysisDataPath.dart
@@ -1,14 +1,12 @@
-import 'package:sw_project/src/models/SocnetoAnalyser.dart';
-
 class AnalysisDataPath {
 
   String analyserId;
-  AnalysisProperty property;
+  String property;
 
   AnalysisDataPath(this.analyserId, this.property);
 
   AnalysisDataPath.fromMap(Map data) :
       analyserId = data["analyser_id"],
-      property = AnalysisProperty.fromMap(data["property"]);
+      property = data["property"];
 
 }
