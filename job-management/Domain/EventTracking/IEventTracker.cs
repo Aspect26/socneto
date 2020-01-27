da98fed1430437f57cb9927c4393ad95e307a8ec
Jaroslav Knotek <jaroslav.knotek@datamole.cz>
2020-01-27 20:30:20 +0100

    add jms event tracking

---
+++ job-management/Domain/EventTracking/IEventTracker.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.EventTracking
+{
+
+    public interface IEventTracker<T>
+    {
+        public void TrackStatistics(string eventName, object serializableAttributes = null);
+        public void TrackInfo(string eventName, string message, object serializableAttributes = null);
+        public void TrackWarning(string eventName, string message, object serializableAttributes=null);
+        public void TrackError(string eventName, string message, object serializableAttributes = null);
+        public void TrackFatal(string eventName, string message, object serializableAttributes = null);
+    }
+
+}
