bd7db4634e3b5f1b0f0146b9f04bdc396de5e5a2
Julius Flimmel <flimmelj@gmail.com>
2020-01-31 15:36:51 +0100

    FE - started implementing quick guide

--- frontend/lib/src/components/app_component/app_layout/workspace/welcome/welcome_component.dart
+++
@@ -1,9 +0,0 @@
-import 'package:angular/angular.dart';
-
-@Component(
-  selector: 'welcome',
-  templateUrl: 'welcome_component.html',
-  styleUrls: ['welcome_component.css'],
-)
-class WelcomeComponent {
-}
