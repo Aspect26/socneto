dc0be2703c3ace19b335fb2ab50de15685ce5ba8
JaroslavKnotek <knotekjaroslav@email.cz>
2020-02-01 16:06:13 +0100

    add event integration

---
+++ backend/Domain/EventTracking/EventSendingHostedService.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Socneto.Domain;
+using Socneto.Domain.Models;
+
+namespace Domain.EventTracking
+{
+    public class EventSendingHostedService : IHostedService
+    {
+        private readonly SystemMetricsOptions _systemMetricsOptions;
+        private readonly IMessageBrokerProducer _messageBrokerProducer;
+        private readonly EventQueue _eventQueue;
+        private readonly ILogger<EventSendingHostedService> _logger;
+        private Task _loggingTask;
+
+        private readonly CancellationTokenSource _cancellationTokenSource
+            = new CancellationTokenSource();
+        public EventSendingHostedService(
+            IMessageBrokerProducer messageBrokerProducer,
+            EventQueue eventQueue,
+            IOptions<SystemMetricsOptions> options,
+            ILogger<EventSendingHostedService> logger)
+        {
+            _systemMetricsOptions = options.Value;
+            _messageBrokerProducer = messageBrokerProducer;
+            _eventQueue = eventQueue;
+            _logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _loggingTask = LogAsync(_cancellationTokenSource.Token);
+            _logger.LogInformation("Logging service started");
+            return Task.CompletedTask;
+        }
+
+        public async Task LogAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                using (var ct = new CancellationTokenSource(TimeSpan.FromSeconds(5)))
+                {
+                    var item = _eventQueue.Dequeue(ct.Token);
+                    if (item == null)
+                    {
+                        await Task.Delay(TimeSpan.FromSeconds(5));
+                    }
+                    else
+                    {
+                        var json = JsonConvert.SerializeObject(item);
+                        var message = new Message { Key = "message-key-1", Value = json };
+                        await _messageBrokerProducer.ProduceAsync(
+                            _systemMetricsOptions.SystemMetricsChannelName
+                            , message);
+                    }
+                }
+            }
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Logging service is stopping");
+            _cancellationTokenSource.Cancel();
+            await _loggingTask;
+            _logger.LogInformation("Logging service stopped");
+        }
+    }
+}
--- backend/Infrastructure/Kafka/KafkaProducer.cs
+++ backend/Infrastructure/Kafka/KafkaProducer.cs
@@ -8,14 +8,12 @@ using Socneto.Domain.Models;
 
 namespace Socneto.Infrastructure.Kafka
 {
-    public class KafkaProducer:IResultProducer
+    public class KafkaProducer : IMessageBrokerProducer
     {
         private readonly ILogger<KafkaProducer> _logger;
         private readonly string _serverAddress;
-        
-        private readonly string _produceTopic;
 
-        public KafkaProducer(IOptions<KafkaOptions> kafkaOptionsObject, IOptions<TaskOptions> taskOptionObject, ILogger<KafkaProducer> logger)
+        public KafkaProducer(IOptions<KafkaOptions> kafkaOptionsObject, ILogger<KafkaProducer> logger)
         {
 
             if (string.IsNullOrEmpty(kafkaOptionsObject.Value.ServerAddress))
@@ -23,31 +21,36 @@ namespace Socneto.Infrastructure.Kafka
             _logger = logger;
             _serverAddress = kafkaOptionsObject.Value.ServerAddress;
 
-
-
-            if (string.IsNullOrEmpty(taskOptionObject.Value.ProduceTopic))
-                throw new ArgumentNullException(nameof(taskOptionObject.Value.ProduceTopic));
-
-            _produceTopic = taskOptionObject.Value.ProduceTopic;
-
-
         }
-        public async Task ProduceAsync(Message message)
+        public async Task ProduceAsync(string channelName, Message message)
         {
             var config = new ProducerConfig { BootstrapServers = _serverAddress };
 
-            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            var producerBuilder = new ProducerBuilder<string, string>(config);
+            producerBuilder.SetErrorHandler(
+                (producer, error) => { _logger.LogError(error.Reason); }
+            );
+
+
+            using (var producer = producerBuilder.Build())
             {
+
                 try
                 {
                     // Note: Awaiting the asynchronous produce request below prevents flow of execution
                     // from proceeding until the acknowledgement from the broker is received (at the 
                     // expense of low throughput).
+
                     var deliveryReport = await producer.ProduceAsync(
-                        _produceTopic, KafkaMessage.ToKafka(message)
+                        channelName, KafkaMessage.ToKafka(message)
                         );
 
-                    _logger.LogInformation($"delivered to: {deliveryReport.TopicPartitionOffset}");
+                    //producer.Produce(
+                    //    channelName, KafkaMessage.FromMessageBrokerMessage(message)
+                    //);
+
+
+                    //_logger.LogInformation($"delivered to: {deliveryReport.TopicPartitionOffset}");
                 }
                 catch (ProduceException<string, string> e)
                 {
@@ -56,5 +59,4 @@ namespace Socneto.Infrastructure.Kafka
             }
         }
     }
-
 }
