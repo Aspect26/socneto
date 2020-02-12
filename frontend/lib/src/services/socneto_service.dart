3ae0b7570ff8e8666c795a422ee466890eea45b6
Julius Flimmel <flimmelj@gmail.com>
2020-02-12 16:36:04 +0100

    FE - fixed csv export

--- frontend/lib/src/services/socneto_service.dart
+++ frontend/lib/src/services/socneto_service.dart
@@ -50,8 +50,8 @@ class SocnetoService {
       await this._dataService.getUserJobs();
   Future<PaginatedPosts> getJobPosts(String jobId, int page, int pageSize, List<String> containsWords, List<String> excludeWords, DateRange dateRange) async =>
       await this._dataService.getJobPosts(jobId, page, pageSize, containsWords, excludeWords, dateRange);
-  String getJobPostsExportLink(String jobId) =>
-      this._dataService.getJobPostsExportLink(jobId);
+  String getJobPostsExportLink(String jobId, List<String> containsWords, List<String> excludeWords, DateRange dateRange) =>
+      this._dataService.getJobPostsExportLink(jobId, containsWords, excludeWords, dateRange);
   Future<List<List<List<dynamic>>>> getChartData(String jobId, ChartDefinition chart) async =>
       await this._dataService.getChartData(jobId, chart);
   Future<List<ChartDefinition>> getJobChartDefinitions(String jobId) async =>
