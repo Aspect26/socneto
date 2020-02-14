0d89aa4e86baee545218739b2e4dd1bf854c88d1
Julius Flimmel <flimmelj@gmail.com>
2020-02-14 16:09:24 +0100

    Moved docker-compose files back to the root directory

--- scripts/run-bohm.sh
+++ scripts/run-bohm.sh
@@ -1,1 +1,1 @@
-docker-compose -f ./docker/docker-compose.yml -f ./docker/docker-compose-bohm.yml up --build --force-recreate
+docker-compose -f ./docker-compose.yml -f ./docker-compose-bohm.yml up --build --force-recreate
