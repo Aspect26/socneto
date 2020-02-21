95700a2b7d666cd8ccdf3307878cdd6d00e7edbc
Julius Flimmel <flimmelj@gmail.com>
2020-02-21 08:57:05 +0100

    FE - display authorId and language for posts

--- frontend/lib/src/models/Post.dart
+++ frontend/lib/src/models/Post.dart
@@ -2,13 +2,17 @@ class Post {
   final String originalId;
   final String text;
   final String originalText;
+  final String authorId;
+  final String language;
   final DateTime postedAt;
 
-  Post(this.originalId, this.text, this.originalText, this.postedAt);
+  Post(this.originalId, this.text, this.originalText, this.authorId, this.language, this.postedAt);
 
   Post.fromMap(Map data) :
       originalId = data["original_id"],
       text = data["text"],
       originalText = data["original_text"],
+      authorId = data["author_id"],
+      language = data["language"],
       postedAt = data["posted_at"] != null? DateTime.parse(data["posted_at"]) : null;
 }
