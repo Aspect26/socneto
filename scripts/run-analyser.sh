81f218bff00361f0b79f059ec2ece88f5b4df33c
Julius Flimmel <flimmelj@gmail.com>
2020-02-21 08:36:41 +0100

    DEPLOY - corrected environment and deployment scripts names

--- scripts/run-analyser.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./docker-compose.yml -f ./docker-compose-analyser.yml up --build --force-recreate -d
--- scripts/run-bohm.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f ./docker-compose.yml -f ./docker-compose-bohm.yml up --build --force-recreate -d
