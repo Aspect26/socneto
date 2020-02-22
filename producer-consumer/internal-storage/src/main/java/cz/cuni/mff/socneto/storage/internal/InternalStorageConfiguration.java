b7a9103611af7b5223742cf47c644f8069580d8a
Lukas Kolek <luky.kolek@seznam.cz>
2020-02-22 22:31:53 +0100

    Storage: removed producer-consumer

--- producer-consumer/internal-storage/src/main/java/cz/cuni/mff/socneto/storage/internal/InternalStorageConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cz.cuni.mff.socneto.storage.internal;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Profile("INTERNAL")
-@Configuration
-public class InternalStorageConfiguration {
-}
