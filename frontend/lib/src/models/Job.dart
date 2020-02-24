207263167b812bffbae249ff7db09686135b3318
Julius Flimmel <flimmelj@gmail.com>
2020-02-24 15:53:37 +0100

    FE+BE - updated 'get all jobs' query so that it now returns also 'topic_query'

--- frontend/lib/src/models/Job.dart
+++ frontend/lib/src/models/Job.dart
@@ -5,17 +5,19 @@ import 'JobStatusCode.dart';
 class Job {
   final String id;
   final String name;
+  final String topicQuery;
   JobStatusCode status;
   final DateTime startedAt;
   final DateTime finishedAt;
 
-  Job(this.id, this.name, this.status, this.startedAt, this.finishedAt);
+  Job(this.id, this.name, this.topicQuery, this.status, this.startedAt, this.finishedAt);
 
   Job.fromMap(Map data) :
-        id = data["jobId"] ?? "",
-        name = data["jobName"] ?? "Unnamed",
+        id = data["job_id"] ?? "",
+        name = data["job_name"] ?? "Unnamed",
+        topicQuery = data["topic_query"] ?? "",
         status = getEnumByString(JobStatusCode.values, data["status"], JobStatusCode.Running),
-        startedAt = data["startedAt"] != null ? DateTime.parse(data["startedAt"]) : null,
-        finishedAt = data["finishedAt"] != null ? DateTime.parse(data["finishedAt"]) : null;
+        startedAt = data["started_at"] != null ? DateTime.parse(data["started_at"]) : null,
+        finishedAt = data["finished_at"] != null ? DateTime.parse(data["finished_at"]) : null;
 
 }
