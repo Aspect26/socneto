15c91407f8ef2398fb8e1796bc72008858d8e5d1
Julius Flimmel <flimmelj@gmail.com>
2020-02-24 15:53:37 +0100

    FE+BE - updated 'get all jobs' query so that it now returns also 'topic_query'

--- backend/Api/Models/JobDto.cs
+++ backend/Api/Models/JobDto.cs
@@ -7,15 +7,23 @@ namespace Socneto.Api.Models
 {
     public class JobDto
     {
+        [JsonProperty("job_id")]
         public Guid JobId { get; set; }
         
+        [JsonProperty("job_name")]
         public string JobName { get; set; }
         
+        [JsonProperty("topic_query")]
+        public string TopicQuery { get; set; }
+        
+        [JsonProperty("status")]
         [JsonConverter(typeof(StringEnumConverter))]
         public  JobStatusCode Status { get; set; }
         
+        [JsonProperty("started_at")]
         public DateTime StartedAt { get; set; }
         
+        [JsonProperty("finished_at")]
         public DateTime? FinishedAt { get; set; }
 
         public static JobDto FromModel(Job job)
@@ -24,6 +32,7 @@ namespace Socneto.Api.Models
             {
                 JobId = job.JobId,
                 JobName =  job.JobName,
+                TopicQuery = job.TopicQuery,
                 Status = job.Status,
                 StartedAt =  job.StartedAt,
                 FinishedAt = job.FinishedAt,
